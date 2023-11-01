22031bd29ae2f5210f4d80d982836604eddef025
Jasmeet Singh <jassoffline586@gmail.com>
2023-11-01 16:02:19 +0530

    Initial Commit

---
+++ components/Logo.tsx
@@ -0,0 +1,18 @@
+import LogoImage from '@logos/logo.png';
+import Link from 'next/link';
+import { AspectRatio } from './ui/aspect-ratio';
+import Image from 'next/image';
+
+function Logo() {
+  return (
+    <Link href='/' prefetch={false} className='overflow-hidden'>
+      <div className='flex items-center w-72 h-14'>
+        <AspectRatio ratio={16 / 9} className='flex items-center justify-center'>
+          <Image priority src={LogoImage} alt='Logo' className='rounded-full dark:filter dark:invert' />
+        </AspectRatio>
+      </div>
+    </Link>
+  );
+}
+
+export default Logo;
