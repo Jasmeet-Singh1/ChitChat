986f927b5e7111a7b1928ca38abdb8eb6267bb06
Jasmeet Singh <jassoffline586@gmail.com>
2023-11-07 14:31:45 +0530

    Home Page is Ready

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,8 @@
 import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
 import './globals.css';
 import Header from '@/components/Header';
 import { ThemeProvider } from '@/components/ui/ThemeProvider';
 import ClientProviders from '@/components/ui/ClientProviders';
-
-const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -16,7 +13,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <ClientProviders>
       <html lang='en'>
-        <body className={inter.className}>
+        <body className='flex flex-col min-h-screen'>
           <ThemeProvider attribute='class' defaultTheme='system' enableSystem disableTransitionOnChange>
             <Header />
             {children}
--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,6 @@
 import Image from 'next/image';
+import Link from 'next/link';
+import DemoGif from '@/images/landingPage/demo.gif';
 
 export default function Home() {
   return (
@@ -11,7 +13,59 @@ export default function Home() {
               clipPath:
                 'polygon (74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0. 1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
             }}
-          ></div>
+          />
+        </div>
+
+        <div className='py-12 sm:py-20 lg:pb-40'>
+          <div className='mx-auto max-w-7xl px-6 lg:px-8'>
+            <div className='mx-auto max-w-2xl text-center'>
+              <h1 className='text-4xl font-bold tracking-tight sm:text-6xl'>Chat with Anyone, anywhere!</h1>
+
+              <p className='mt-6 text-lg leading-8 text-gray-600 dark:text-gray-300'>
+                You speak your language, they speak their language.{' '}
+                <span className='text-indigo-600 dark: text-indigo-500'>Let AI handle the translation.</span>
+              </p>
+              <div className='mt-10 flex items-center justify-center gap-x-6'>
+                <Link
+                  href='/chat'
+                  className='rounded-md bg-indigo-600 Tx-3.5 py-2.5 text-sm font-semibold text-white dark: text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible: outline-indigo-600'
+                >
+                  Get started
+                </Link>
+                <Link
+                  href='/pricing'
+                  className='text-sm font-semibold leading-6 text-gray-900  dark:text-gray-300'
+                >
+              View Pricing →   <span aria-hidden='true'></span>
+                </Link>
+              </div>
+            </div>
+            <div className='mt-16 flow-root sm:mt-24'>
+              <div className='-m-2 rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:-m-4 lg:rounded-2xl lg:p-4'>
+                <Image
+                  unoptimized
+                  src={DemoGif}
+                  alt='App screenshot'
+                  width={2432}
+                  height={1442}
+                  className='rounded-md shadow-2xl ring-1 ring-gray-900/10'
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div
+          className='absolute inset-x-0 top-[calc(100 %-13 rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm: top-[calc (100%-30 rem)]'
+          aria-hidden='true'
+        >
+          <div
+            className='relative left-[calc(50% +3 rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm: left-[calc(50%+36 rem)] smw-[72.1875 rem]'
+            style={{
+              clipPath:
+                'polygon (74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0. 1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
+            }}
+          />
         </div>
       </div>
     </main>
